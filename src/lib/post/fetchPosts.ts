eec30e810c6c70950ca7a9e86b6bd40575df3b42
Cael <MR-Addict@qq.com>
2024-07-24 10:00:26 +0800

    feat: update blog post url

--- src/lib/post/fetchPosts.ts
+++ src/lib/post/fetchPosts.ts
@@ -3,6 +3,6 @@ import z from "zod";
 import { Post } from "@/types/post";
 
 export default async function fetchPosts() {
-  const data = await fetch("https://playground.mraddict.top/api/blog/latest").then((res) => res.json());
+  const data = await fetch("https://playground.mraddict.vercel.app/api/blog/latest").then((res) => res.json());
   return z.array(Post).parse(data);
 }
